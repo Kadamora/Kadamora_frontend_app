3543f4a59e11eee8619ce16b99e4964cb721ac4b
JudeChukwuma14 <ebukajude14@gmail.com>
2026-01-07 07:19:03 +0100

    BrowserRouter remove

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,16 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Preloader from "@components/Preloader/Preloader.tsx";
 import { persistor, store } from "@store/index.ts";
-import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate
-          loading={<Preloader minLoadingTime={800} />}
-          persistor={persistor}
-        >
-          <App />
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate
+        loading={<Preloader minLoadingTime={800} />}
+        persistor={persistor}
+      >
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
