246f8703a912764959677a9f02c02765112c5f5b
JudeChukwuma14 <ebukajude14@gmail.com>
2026-01-07 04:38:49 +0100

    browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import Preloader from '@components/Preloader/Preloader.tsx';
-import { persistor, store } from '@store/index.ts';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import Preloader from "@components/Preloader/Preloader.tsx";
+import { persistor, store } from "@store/index.ts";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-< Provider store={store}>
-            <PersistGate loading={<Preloader minLoadingTime={800} />} persistor={persistor}>
-                <App />
-            </PersistGate>
-        </Provider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate
+          loading={<Preloader minLoadingTime={800} />}
+          persistor={persistor}
+        >
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
